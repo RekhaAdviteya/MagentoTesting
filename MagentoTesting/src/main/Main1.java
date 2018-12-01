d1c564fd7c605b07b5ef1858dc6b82d8f4dda4cf
Rekha <Rekha@DESKTOP-9GIM85F>
2018-12-01 16:48:23 +0530

    My first maven project

---
+++ MagentoTesting/src/main/Main1.java
@@ -0,0 +1,19 @@
+package com.abc.magentoObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Main1
+{	
+	WebDriver driver;
+By logout=By.linkText("Log Out");
+public Main1(WebDriver driver) 
+{
+	this.driver=driver;
+}
+public void clickOnLogOut()
+{
+	driver.findElement(logout).click();
+}
+
+}
